0590f7c8bb87269f267e60d10427df56cca874a8
Edward Parada <edwardpara910@gmail.com>
2023-03-16 01:14:45 -0500

    Huge message optimizations, no loger using innerHTML

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -35,19 +35,26 @@ export class chatPage implements OnInit {
   public firstMessageIndex = 0;
   public messages: Message[] = [];
   public lst!: string[];
-  private isLoading = false;
+  private isLoading!: boolean;
   private hasManuallyScrolled = false;
   private maxTokenUsage = 3096;
   private lastMessageId! : string;
   private whiteHighlight = '#ffffff40';
 
   @ViewChild('textbox', { static: false }) textbox!: ElementRef;
-  @ViewChild('conversation', { static: false }) conversation!: ElementRef;
+  @ViewChild('conversation', { static: false }) conversation!: ElementRef<HTMLElement>;
   @ViewChild('downButton', { static: false, read: ElementRef  }) downButton!: ElementRef;
+  @ViewChild('stopButton', { static: false, read: ElementRef  }) stopButton!: ElementRef;
 
-  constructor(private activatedRoute: ActivatedRoute, private AiService: AiService, private router: Router, private toastController: ToastController) { }
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private AiService: AiService, 
+    private router: Router, 
+    private toastController: ToastController,
+    ) { }
 
   ngOnInit() {
+
     this.id = this.activatedRoute.snapshot.paramMap.get('id') as string;
     const data = localStorage.getItem(this.id);
 
@@ -136,14 +143,123 @@ export class chatPage implements OnInit {
       }
     }, 100);
 
+    // Load messages
+    this.loadMessages();
+  }
+  
+  ionViewDidEnter(){
     setTimeout(() => {
-      if (this.firstMessageIndex != 0){
-        this.showFirstMessage();
-      }
-      textBoxNative.focus();
+      this.textbox.nativeElement.focus();
       this.conversation.nativeElement.scrollTop = this.conversation.nativeElement.scrollHeight;
+      // this.conversation.nativeElement.scrollTo({ top: this.conversation.nativeElement.scrollHeight, behavior: 'smooth'})
     }, 100);
     
+    this.stopLoading()
+  }
+
+  setLoading(){
+    this.isLoading = true
+    this.stopButton.nativeElement.disabled = false;
+  }
+
+  stopLoading(){
+    this.isLoading = false
+    this.stopButton.nativeElement.disabled = true;
+    // this.stopButton.nativeElement.style.color = '#ffffff40';
+  }
+
+  loadMessages() {
+    const parentElement = this.conversation.nativeElement;
+
+    for (let i = 0; i < this.messages.length; i++) {
+
+      const messageBubble = this.processMessageContent(this.messages[i]);
+
+      if (i == this.firstMessageIndex - 1) {
+        this.processFirstMessage(messageBubble);
+      }
+
+      parentElement.appendChild(messageBubble);
+      
+    }
+
+  }
+
+  processMessageContent(message: Message) {
+      const messageBubble = document.createElement('div');
+      messageBubble.classList.add('message-bubble');
+      messageBubble.classList.add(message.role == 'user' ? 'user-bubble' : 'assistant-bubble');
+
+      const text = message.content;
+      const dividedText = text.split('```');
+
+      for (let j = 0; j < dividedText.length; j++) {
+        const textPart = dividedText[j];
+
+        if (j % 2 == 1) {
+          messageBubble.appendChild(this.processCodeBlock(textPart));
+        } else {
+          messageBubble.appendChild(this.processTextBlock(textPart));
+        }
+      }
+
+      const codeBlock = document.createElement('div');
+      codeBlock.classList.add('code');
+
+      return messageBubble;
+  }
+
+  processCodeBlock(codeText: string){
+    const codeBlock = document.createElement('div');
+    codeBlock.classList.add('code');
+    codeBlock.classList.add('code-text');
+    // textBlock.classList.add('text');
+    // codeBlock.innerText = codeText;
+
+    const headerRegex = /^(\w*)\n(.+)/s;
+    const blockHeader = document.createElement('div');
+    const blockContent = document.createElement('div');
+    const regex = codeText.match(headerRegex);
+
+    blockHeader.classList.add('code-header');
+    blockContent.classList.add('code-content');
+
+    if (regex){
+      blockHeader.innerText = regex[1]? regex[1] : 'code';
+      blockContent.innerText = regex[2];
+    } else {
+      blockHeader.innerText = 'code';
+      blockContent.innerText = codeText;
+    }
+
+    codeBlock.appendChild(blockHeader);
+    codeBlock.appendChild(blockContent);
+
+    return codeBlock;
+  }
+
+  processTextBlock(text: string){
+    const textBlock = document.createElement('p');
+
+    const textChunks = text.split('`');
+
+    for (let i = 0; i < textChunks.length; i++){
+      const textSpan = document.createElement('span');
+
+      if (i % 2 == 1) {
+        textSpan.classList.add('code-text');
+      }
+      
+      textSpan.innerText = textChunks[i];
+      textBlock.appendChild(textSpan);
+    }
+
+    return textBlock;
+  }
+
+  processFirstMessage(messageBubble: HTMLElement){
+    messageBubble.style.borderBottom = `1px solid ${this.whiteHighlight}`;
+    // messageBubble.style.paddingBottom = '30px';
   }
 
   showFirstMessage() {
@@ -154,8 +270,7 @@ export class chatPage implements OnInit {
       lastBubble = messageBubbles[this.firstMessageIndex - 2] as HTMLElement;
     }
 
-    lastBubble.style.borderBottom = `1px solid ${this.whiteHighlight}`;
-    lastBubble.style.paddingBottom = '30px';
+    this.processFirstMessage(lastBubble);
   }
 
   getSessionData() {
@@ -176,23 +291,19 @@ export class chatPage implements OnInit {
     return messagesContext;
   }
 
-  handleExample() {
-    alert('hola');
-  }
-
   handleDownAction() {
     this.conversation.nativeElement.scrollTop = this.conversation.nativeElement.scrollHeight;
     this.hasManuallyScrolled = false;
   }
 
   handleStopAction() {
-    this.isLoading = false;
-    this.addCopyButton()
+    this.stopLoading();
   }
 
   handleClearAction() {
     this.handleStopAction();
     // this.firstMessageIndex = this.messages? this.messages.length - 1 : 0;
+    this.presentToast('Context cleared', 1000, 'top', 'dark-toast');
     
     if (this.firstMessageIndex == this.messages.length){
       return;
@@ -202,7 +313,6 @@ export class chatPage implements OnInit {
     this.saveSessionData();
     this.showFirstMessage();
     this.conversation.nativeElement.scrollTop = this.conversation.nativeElement.scrollHeight;
-    this.presentToast('Context cleared', 1000, 'top');
   }
 
   async handleSentAction() {
@@ -215,23 +325,32 @@ export class chatPage implements OnInit {
       return;
     }
 
-    const userMessage = { role: 'user', content: this.textbox.nativeElement.value };
-    const AImessage = { role: 'assistant', content: '' };
-    let index = 0;
-    this.textbox.nativeElement.value = '';
+    const userMessage = { role: 'user', content: this.textbox.nativeElement.value.trim() };
+    const AIMessage = { role: 'assistant', content: '' };
 
+    const userMessageBubble = this.processMessageContent(userMessage);
+    const AIMessageBubble = this.processMessageContent(AIMessage);
+
+    this.conversation.nativeElement.appendChild(userMessageBubble);
+    this.conversation.nativeElement.appendChild(AIMessageBubble);
+
+    this.textbox.nativeElement.value = '';
+    
     this.messages.push(userMessage);
     this.saveSessionData();
 
-    this.messages.push(AImessage);
-
-    this.isLoading = true;
+    this.messages.push(AIMessage);
+    
     this.hasManuallyScrolled = false;
+    this.setLoading()
+    
+    let index = 0;
     let currentId : string = '';
-
+    
     try {
 
-      await this.AiService.callAPI(this.pruneMessages([this.AiService.getSystemMessage(), ...this.extractContext().slice(0, -1)]),
+      await this.AiService.callAPI(
+        this.pruneMessages([this.AiService.getSystemMessage(), ...this.extractContext().slice(0, -1)]),
         (pe: ProgressEvent) => {
           if (!this.isLoading) {
             return
@@ -252,14 +371,8 @@ export class chatPage implements OnInit {
             //   console.log(rawData);
             // }
 
-            if (!rawData) {
-              this.isLoading = true;
-              return;
-            }
-
             if (rawData === '[DONE]') {
               console.log('Finished stream');
-              // this.isLoading = false;
               return;
             }
 
@@ -288,18 +401,20 @@ export class chatPage implements OnInit {
 
             // If the stream has finished, stop the completion
             if (data.choices[0]?.finish_reason){
-              this.isLoading = false;
+              this.stopLoading();
               console.log('Stopped completion');
               return;
             }
 
             // If it has not stopped, add text to the messagle bubble
             const chunkContent = data.choices[0]?.delta.content;
-            console.log(chunkContent)
+            // console.log(chunkContent)
 
             if (chunkContent && data.id == this.lastMessageId) {
-              AImessage.content += chunkContent;
-              AImessage.content = AImessage.content.charAt(0).toUpperCase() + AImessage.content.slice(1);
+              AIMessage.content += chunkContent;
+              AIMessage.content = AIMessage.content.charAt(0).toUpperCase() + AIMessage.content.slice(1);
+
+              AIMessageBubble.innerHTML = this.processMessageContent(AIMessage).innerHTML;
             }
           }
 
@@ -335,45 +450,6 @@ export class chatPage implements OnInit {
     }
 
     return messages;
-  }
-
-  processText(text: string) {
-    var processedText = text;
-
-    // Prevenir que se renderize HTML
-    processedText = processedText.replace(/</g, '<span><</span>');
-
-    // Cuadros especiales para codigo
-    const incompleteCodeRegex = /```(.*)\n*([\S\s]+)/g;
-    const codeRegex = /```(.*)\n*([\s\S]+?)```/g;
-    const codeReplacement = '<div class="code"> <div class="code-header">$1</div> <div class="code-content">$2</div></div>';
-
-    processedText = processedText.replace(codeRegex, codeReplacement);
-    processedText = processedText.replace(incompleteCodeRegex, codeReplacement);
-    processedText = processedText.replace('<div class="code-header"></div>', '<div class="code-header">code</div>');
-
-    // this.addCopyButton()
-    return processedText;
-  }
-
-  addCopyButton() {
-    // const elements = document.querySelectorAll('.code-header');
-    const elements = document.querySelectorAll('.user-bubble');
-
-    elements.forEach((element) => {
-      if (!element.hasAttribute('data-button-added')) {
-        let button = document.createElement('ion-button');
-        button.textContent = 'Click me';
-
-        // Add event listener to button here...
-        button.addEventListener('click', () => {
-          alert('Button clicked!');
-        });
-        // element.append(button);
-        // element.insertAdjacentHTML('afterend', '<ion-button>Click me</ion-button>');
-        element.setAttribute('data-button-added', 'true');
-      }
-    });
   }
 
   // Add title to chat on first message
@@ -482,15 +558,16 @@ export class chatPage implements OnInit {
     this.messages.push({ role: 'assistant', content: '```API error\n' + JSON.stringify(data.error, null, 2) + '```' });
   }
 
-  async presentToast(message: string, duration: number, position: 'top' | 'middle' | 'bottom') {
+  async presentToast(message: string, duration: number, position: 'top' | 'middle' | 'bottom', cssClass?: string) {
     const toast = await this.toastController.create({
       message: message,
       duration: duration? duration : 1000,
       position: position,
+      cssClass: cssClass,
       buttons: [
         {
           text: 'Dismiss',
-          role: 'cancel'
+          role: 'cancel',
         }
       ],
     });
